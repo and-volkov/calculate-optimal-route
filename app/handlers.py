6d82b95831949dddc4e245fdfbaf56dcd6c2a9c2
Andrey Volkov <andrey.dreamwall@gmail.com>
2024-04-01 10:36:51 +0400

    update

--- app/handlers.py
+++ app/handlers.py
@@ -1,5 +1,4 @@
 import csv
-import heapq
 import logging
 import math
 from io import StringIO
@@ -39,6 +38,52 @@ class TSPHandler:
         self.file.file.close()
         return coordinates
 
+    def distance(
+        self, coord1: dict[str, float], coord2: dict[str, float]
+    ) -> float:
+        """
+        Calculate the Euclidean distance between two coordinates
+
+        Args:
+            coord1: A dictionary with "lat" and "lng" keys
+            coord2: A dictionary with "lat" and "lng" keys
+
+        Return:
+            The Euclidean distance between the two coordinates
+        """
+        return math.sqrt(
+            (coord1["lat"] - coord2["lat"]) ** 2
+            + (coord1["lng"] - coord2["lng"]) ** 2
+        )
+
+    def nearest_neighbor(self, coordinates) -> list[int]:
+        """
+        Find the nearest neighbor of each coordinate
+
+        Args:
+            coords: A list of dictionaries with "lat" and "lng" keys
+
+        Return:
+            A list of indexes representing the optimal path
+        """
+        unvisited = set(range(len(coordinates)))
+        current_point = 0  # Start from the first point
+        path = [current_point]
+        unvisited.remove(current_point)
+
+        while unvisited:
+            nearest_point = min(
+                unvisited,
+                key=lambda x: self.distance(
+                    coordinates[current_point], coordinates[x]
+                ),
+            )
+            path.append(nearest_point)
+            unvisited.remove(nearest_point)
+            current_point = nearest_point
+
+        return path
+
     def sort_coordinates(
         self, coordinates: list[dict[str, float]]
     ) -> list[dict[str, float]]:
@@ -46,58 +91,13 @@ class TSPHandler:
         Sort a list of coordinates by latitude and longitude
 
         Args:
-            A list of indexes representing the optimal path
+        A list of indexes representing the optimal path
 
         Returns:
-            A sorted list of dictionaries
+        A sorted list of dictionaries
         """
         optimal_path = self.nearest_neighbor(coordinates)
         return [coordinates[i] for i in optimal_path]
-
-    def distance(
-        self, coord1: dict[str, float], coord2: dict[str, float]
-    ) -> float:
-        """
-        Calculate the Euclidean distance between two coordinates
-        """
-        return math.sqrt(
-            (coord1["lat"] - coord2["lat"]) ** 2
-            + (coord1["lng"] - coord2["lng"]) ** 2
-        )
-
-    def nearest_neighbor(self, coords: list[dict[str, float]]) -> list[int]:
-        """
-        Find the nearest neighbor for each point in a list of coordinates
-
-        Args:
-            coords: A list of dictionaries with "lat" and "lng" keys
-
-        Returns:
-            A list of indexes representing the optimal path
-        """
-        unvisited = set(range(len(coords)))
-        current_point = 0  # Start from the first point
-        path = [current_point]
-        unvisited.remove(current_point)
-
-        pq = [
-            (self.distance(coords[current_point], coords[i]), i)
-            for i in unvisited
-        ]
-        heapq.heapify(pq)
-
-        while pq:
-            _, nearest_point = heapq.heappop(pq)
-            path.append(nearest_point)
-            unvisited.remove(nearest_point)
-
-            pq = [
-                (self.distance(coords[nearest_point], coords[i]), i)
-                for i in unvisited
-            ]
-            heapq.heapify(pq)
-
-        return path
 
     def process(self) -> list[dict[str, float]]:
         """
